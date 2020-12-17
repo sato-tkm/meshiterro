8bfef695aed75c2bfe5a8764f1f2aca78775ff3f
sato-tkm <t.28pnorudo@gmail.com>
2020-12-17 09:29:18 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3f35cfd06eb1ebc1e0eb64173386fb9e711420200ba896ef0f285d35107363fc4eeb01b2377c9d4f5d6b99103913aa85effd717e12e6e59b0e8d4830a7103ca1'
